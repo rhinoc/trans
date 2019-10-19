7065731301804d2b6cd75dd96164cf71305566a5
rhinoc <rhinoc@outlook.com>
2019-10-19 20:39:57 +0800

    Initial Commit

---
+++ trans/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  trans
+//
+//  Created by Seon Wong on 2019/10/19.
+//  Copyright © 2019 Seon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
