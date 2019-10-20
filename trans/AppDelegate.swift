f8790b923b6d218d2792f50448ac447b34861314
rhinoc <rhinoc@outlook.com>
2019-10-20 16:48:22 +0800

    commit message

---
+++ trans/AppDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  AppDelegate.swift
+//  trans
+//
+//  Created by Seon Wong on 2019/10/19.
+//  Copyright © 2019 Seon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var timer: Timer!
+    let pasteboard: NSPasteboard = .general
+    var lastChangeCount: Int = 0
+    
+    @IBOutlet weak var popover: NSPopover!
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        showPopover(popover)
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("statusIcon"))
+            button.action = #selector(togglePopover)
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (t) in
+            if self.lastChangeCount != self.pasteboard.changeCount {
+                self.lastChangeCount = self.pasteboard.changeCount
+                NotificationCenter.default.post(name: .NSPasteboardDidChange, object: self.pasteboard)
+            }
+        }
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        timer.invalidate()
+    }
+    
+    @objc func quitApp(_ sender: AnyObject) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    @objc func showPopover(_ sender: AnyObject) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+        
+    @objc func closePopover(_ sender: AnyObject) {
+        popover.performClose(sender)
+    }
+        
+    @objc func togglePopover(_ sender: AnyObject) {
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+
+}
+
+extension NSNotification.Name {
+    public static let NSPasteboardDidChange: NSNotification.Name = .init(rawValue: "pasteboardDidChangeNotification")
+}
+
+extension NSTextField {
+    open override func performKeyEquivalent(with event: NSEvent) -> Bool {
+        if event.modifierFlags.isDisjoint(with: .command) {
+            return super.performKeyEquivalent(with: event)
+        }
+        
+        switch event.charactersIgnoringModifiers {
+        case "a":
+            return NSApp.sendAction(#selector(NSText.selectAll(_:)), to: self.window?.firstResponder, from: self)
+        case "c":
+            return NSApp.sendAction(#selector(NSText.copy(_:)), to: self.window?.firstResponder, from: self)
+        case "v":
+            return NSApp.sendAction(#selector(NSText.paste(_:)), to: self.window?.firstResponder, from: self)
+        case "x":
+            return NSApp.sendAction(#selector(NSText.cut(_:)), to: self.window?.firstResponder, from: self)
+        default:
+            return super.performKeyEquivalent(with: event)
+        }
+    }
+}
